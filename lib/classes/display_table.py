27ec8eb22b950d2aadbadad7d110520ac5459884
stephy <stephshiro35@gmail.com>
2024-06-10 20:21:18 +0300

    import texttable

--- lib/classes/display_table.py
+++ lib/classes/display_table.py
@@ -1,3 +1,4 @@
 from sqlalchemy import (create_engine, Column, Integer, String)
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
+from texttable import Texttable
