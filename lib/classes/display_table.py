b10e18f237c415f18fdd182ded8cb287ace9bb29
stephy <stephshiro35@gmail.com>
2024-06-10 20:24:17 +0300

    create class display table

--- lib/classes/display_table.py
+++ lib/classes/display_table.py
@@ -2,3 +2,7 @@ from sqlalchemy import (create_engine, Column, Integer, String)
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 from texttable import Texttable
+
+Base = declarative_base()
+
+class DisplayTable(Base):
