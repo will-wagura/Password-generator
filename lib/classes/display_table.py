4d251aaf3071e34a3045c007d108a00af1123dd5
stephy <stephshiro35@gmail.com>
2024-06-10 20:39:45 +0300

    create session object and sessionmaker

--- lib/classes/display_table.py
+++ lib/classes/display_table.py
@@ -21,3 +21,5 @@ if __name__ == '__main__':
 
     engine = create_engine('sqlite:///passwords.db')
     Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
