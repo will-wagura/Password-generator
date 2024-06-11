04df05d8f240e858c3c3df7b25dfe845677e81df
William Ndirangu <wwagura0@gmail.com>
2024-06-11 06:32:24 +0300

    Creates CLI menu options

--- lib/cli.py
+++ lib/cli.py
@@ -2,47 +2,50 @@
 
 from helpers import (exit_program, helper_1, helper_2)
 
+Base = declarative_base()
 
-def main():
-    while True:
-        menu()
-        choice = input("> ")
-        if choice == "0":
-            exit_program()
-        elif choice == "1":
-            helper_1()
-        elif choice == "2":
-            helper_2()
-        else:
-            print("Invalid choice")
+if __name__ == '__main__':
+    engine = create_engine('sqlite:///passwords.db')
+    Base.metadata.create_all(engine)
+    session = sessionmaker(bind=engine)
+    session = Session()
 
-
-def start_screen():
-    print('Welcome to Encrypto.')
-
-def login():
-    print('Please enter your username:')
-    username = 'Group 6'
-    action = input("Enter your username: ")
-    if action != username:
-        print("Invalid username. Please try again.")
-    if action == username:
-        menu()
-
-    print('Please enter your password:')
-    password = '1234'
-    action = input('Enter your password: ') 
-    if action != password:
-        print('Invalid credentials. Please try again.')
-    if action == password:
-        menu()
-    
-def menu():
-    print("Please select an option:")
-    print("0. Exit the program")
-    print("1. Some useful function")
-    print("2. Create new account")
+    def main():
+        while True:
+            menu()
+            choice = input("> ")
+            if choice == "0":
+                exit_program()
+            elif choice == "1":
+                helper_1()
+            elif choice == "2":
+                helper_2()
+            else:
+                print("Invalid choice")
 
 
-if __name__ == "__main__":
-    main()
+    def start_screen():
+        print('Welcome to Encrypto.')
+
+    def login():
+        print('Please enter your username:')
+        print('Please enter your password:') 
+        username = 'Group 6'
+        password = '1234'
+        action = input("Enter your username: ")
+        action = input('Enter your password: ')
+        if action != username and password:
+            print("Invalid username. Please try again.")
+        if action == username and password:
+            menu()
+        
+    def menu():
+        print("Welcome to Encrypto. What would you like to do?:")
+        print('Please select an option:')
+        print("0. Help")
+        print("1. Create a new password")
+        print("2. Manage passwords")
+
+
+start_screen()
+login()
