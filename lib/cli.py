3e7c8f3bcfbe137b1da7334fe913d4fd68799d35
William Ndirangu <wwagura0@gmail.com>
2024-06-11 06:53:54 +0300

    Resolves login and signup logic

--- lib/cli.py
+++ lib/cli.py
@@ -4,6 +4,7 @@ from helpers import (exit_program, helper_1, helper_2)
 
 Base = declarative_base()
 
+#Added some SQLAlchemy imports for the user model to create a template
 if __name__ == '__main__':
     engine = create_engine('sqlite:///passwords.db')
     Base.metadata.create_all(engine)
@@ -26,25 +27,50 @@ if __name__ == '__main__':
 
     def start_screen():
         print('Welcome to Encrypto.')
+        print('1. SignUp')
+        print('2. Login')
+        action = input ("Please select an option: ")
+
+        if action == '1':
+            sign_Up()
+        elif action == '2':
+            login()
+        else:
+            print('Invalid option. Please select a valid option.')
+            start_screen()
+
+    def sign_Up():
+        print('Please enter your desired username:')
+        username = input("> ")
+        print('Please enter your desired password:')
+        password = input("> ")
+
+        #Add logic for SQAlchemy to link signup with the database to store username and password data and create a user model.
+
+        print("Sign up successful! Please Login to continue.")
+        start_screen()
 
     def login():
         print('Please enter your username:')
+        username = input("> ")
         print('Please enter your password:') 
-        username = 'Group 6'
-        password = '1234'
-        action = input("Enter your username: ")
-        action = input('Enter your password: ')
-        if action != username and password:
-            print("Invalid username. Please try again.")
-        if action == username and password:
+        password = input("> ")
+
+        #Use SQAlchemy to query user model and see if the user and password match for now I have hardcoded the username and password. 
+        if username == "Group 6" and password == "1234":
+            print("Login successful!")
             menu()
+        else:
+            print("Invalid username or password. Please try again.")
+            login()
         
     def menu():
         print("Welcome to Encrypto. What would you like to do?:")
-        print('Please select an option:')
         print("0. Help")
         print("1. Create a new password")
         print("2. Manage passwords")
+        print("3. Quit")
+
 
 
 start_screen()
