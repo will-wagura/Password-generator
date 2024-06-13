0b1e6a232c7e8f4f46f672f0cb780f8010ee5817
William Ndirangu <wwagura0@gmail.com>
2024-06-13 04:22:35 +0300

    Addresses errors

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,17 +1,5 @@
 # # lib/helpers.py
 
-# #def helper_1():
-#     #print("Performing useful function#1.")
-
-def helper_2():
-    print("Create new account")
-
-
-# #def exit_program():
-#     #print("Goodbye!")
-#     #exit()
-
-# # helpers.py
 # import string
 # import secrets
 # from getpass import getpass
